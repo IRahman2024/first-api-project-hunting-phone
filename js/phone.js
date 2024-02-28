1421d90deae7430e4886941ead06e686bd9b86fb
IRahman2024 <stormfuryultra784@gmail.com>
2024-02-28 22:09:43 +0600

    first commit

---
+++ js/phone.js
@@ -0,0 +1,43 @@
+const loadPhone = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones);
+    displayPhones(phones);//display function e phones data pass kore
+}
+
+// function loadPhone(){
+//     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+//     .then(res => res.json())
+//     .then(data => console.log(data.data));
+// }
+
+//eikhane forEach function use kore individual data pawar kaj hoise 
+const displayPhones = phones =>{
+
+    //step 1: create point where to add dynamic code
+    const phoneContainer = document.getElementById('phone-container');
+
+
+    phones.forEach(phone =>{
+        console.log(phone);
+        //step 2: create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-gray-500 shadow-xl`;
+        // step 2: set inner html
+        phoneCard.innerHTML = `
+                <figure><img src="${phone.image}" alt="Shoes" /></figure>
+                <div class="card-body">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+                </div>
+        `;
+        //step 4: append html
+        phoneContainer.appendChild(phoneCard);
+    })
+} 
+
+loadPhone();
