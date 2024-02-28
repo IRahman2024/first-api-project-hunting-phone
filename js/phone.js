fe5421e158370637febd283cf0161abfe979decb
IRahman2024 <stormfuryultra784@gmail.com>
2024-02-29 02:36:37 +0600

    search function added

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (brand) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brand}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
@@ -18,13 +18,15 @@ const displayPhones = phones =>{
     //step 1: create point where to add dynamic code
     const phoneContainer = document.getElementById('phone-container');
 
+    //remove old search phone with new one
+    phoneContainer.textContent = '';
 
     phones.forEach(phone =>{
-        console.log(phone);
+        // console.log(phone);
         //step 2: create a div
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-gray-500 shadow-xl`;
-        // step 2: set inner html
+        phoneCard.classList = `card bg-gray-500 shadow-xl p-8`;
+        // step 3: set inner html
         phoneCard.innerHTML = `
                 <figure><img src="${phone.image}" alt="Shoes" /></figure>
                 <div class="card-body">
@@ -40,4 +42,12 @@ const displayPhones = phones =>{
     })
 } 
 
-loadPhone();
+// handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+}
+
+
+// loadPhone();
