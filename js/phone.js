5633f22a29384483995e1bbcd0e5a0d0259411c6
IRahman2024 <stormfuryultra784@gmail.com>
2024-02-29 20:43:35 +0600

    bad work done

--- js/phone.js
+++ js/phone.js
@@ -1,9 +1,9 @@
-const loadPhone = async (brand) =>{
+const loadPhone = async (brand='13', isShowALL) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brand}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
-    displayPhones(phones);//display function e phones data pass kore
+    displayPhones(phones, isShowALL);//display function e phones data pass kore
 }
 
 // function loadPhone(){
@@ -13,7 +13,7 @@ const loadPhone = async (brand) =>{
 // }
 
 //eikhane forEach function use kore individual data pawar kaj hoise 
-const displayPhones = phones =>{
+const displayPhones = (phones, isShowALL) =>{
 
     //step 1: create point where to add dynamic code
     const phoneContainer = document.getElementById('phone-container');
@@ -23,12 +23,15 @@ const displayPhones = phones =>{
 
     //display show all button if there are more then 10 cards
     const showAll = document.getElementById('show-all-container');
-    if(phones.length > 9)
+    if(phones.length > 9 && !isShowALL)
         showAll.classList.remove('hidden');
     else
-    showAll.classList.add('hidden');
-    // display only 10 phones at a time
-    phones = phones.slice(0, 10);
+        showAll.classList.add('hidden');
+    
+    if(!isShowALL){
+        // display only 10 phones at a time
+        phones = phones.slice(0, 10);
+    }
 
     phones.forEach(phone =>{
         // console.log(phone);
@@ -41,22 +44,72 @@ const displayPhones = phones =>{
                 <div class="card-body">
                 <h2 class="card-title">${phone.phone_name}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <div class="card-actions justify-center">
+                <!-- // ${phone.slug} part take quation e rakha hoise karon phone.slug ekta non-string variable return kore-->
+                    <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show detail</button>
                 </div>
                 </div>
         `;
         //step 4: append html
         phoneContainer.appendChild(phoneCard);
-    })
+    });
+    toggleLoadingSpinner(false);
+
+
 } 
 
 // handle search button
-const handleSearch = () => {
+const handleSearch = (isShowALL) => {
+    toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhone(searchText);
+    loadPhone(searchText, isShowALL);
 }
 
+const toggleLoadingSpinner = (isLoading) =>{
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading)
+        loadingSpinner.classList.remove('hidden');
+    else
+        loadingSpinner.classList.add('hidden');
+}
 
-// loadPhone();
+const showAll = () =>{
+    handleSearch(true);
+}
+
+const handleShowDetail = async (id) =>{
+    // console.log('clicked by ', id);
+    //
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    showPhoneDetails(phone);
+}
+
+const showPhoneDetails = (phones) =>{
+    console.log(phones);
+    const phoneName = document.getElementById('show-detail-phone-name');
+    phoneName.innerText = phones.name;
+
+    const showDetailContainer = document.getElementById('show-detail-container');
+
+    showDetailContainer.innerHTML = `
+    <img src="${phones.image}" alt="">
+    <p>storage: ${phones?.mainFeatures.storage}</p>
+    <!-- // etar mane hoilo je jodi gps na thake taile no gps dekhabe  -->
+    <p>storage: ${phones?.others?.gps || 'no gps available'}</p>
+    <!--//this is ternary method -->
+    <p>storage: ${phones?.others?.gps ? phones?.others?.gps : 'no gps available'}</p>
+    
+    `
+
+
+
+
+
+
+    show_detail_modal.showModal()
+}
+
+loadPhone();
